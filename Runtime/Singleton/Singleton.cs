8aa014cb0738cb03574904b786508aa8fe573349
fhps <higino.felipe@hotmail.com>
2020-09-21 10:56:46 -0300

    Update Singleton.cs

--- Runtime/Singleton/Singleton.cs
+++ Runtime/Singleton/Singleton.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+public abstract class Singleton<T> : MonoBehaviour where T : Component
 {
     public static T Instance;
     public void AwakeSingleton(T objectRef)
